db2af7d04203620cf577cffb452cd1549cb55b32
marcothelost <marcothelost@proton.me>
2024-02-01 15:38:54 +0100

    Setting up import aliases

--- src/components/blocks/PageNavigation.tsx
+++ src/components/blocks/PageNavigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { AppRoute } from '../../utils/route';
+import { AppRoute } from '@utils/route';
 
 export const PageNavigation: React.FC = () => (
   <nav>
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 
-import { PageNavigation } from '../components/blocks/PageNavigation';
-import { PageFooter } from '../components/blocks/PageFooter';
+import { PageNavigation } from '@components/blocks/PageNavigation';
+import { PageFooter } from '@components/blocks/PageFooter';
 
-import { PAGE_TITLE_BASE, PAGE_TITLE_SPLITTER } from '../utils/route';
+import { PAGE_TITLE_BASE, PAGE_TITLE_SPLITTER } from '@utils/route';
 
 interface Props {
   children: React.ReactNode;
