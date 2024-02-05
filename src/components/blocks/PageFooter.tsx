63e3a0f85d485e0aeb6e4ae1515e4ec29e30c00f
marcothelost <marcothelost@proton.me>
2024-02-05 20:39:54 +0100

    Creating the Content Wrapper element

--- src/components/blocks/PageFooter.tsx
+++ src/components/blocks/PageFooter.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import { ContentWrapper } from '@components/elements/ContentWrapper';
+
 export const PageFooter: React.FC = () => (
   <footer className="page-footer">
-    <hr />
-    <p>TBOI Lookup &copy; 2024</p>
+    <ContentWrapper>
+      <p className="page-footer__copyright">TBOI Lookup &copy; 2024</p>
+    </ContentWrapper>
   </footer>
 );
